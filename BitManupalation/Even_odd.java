2030cf7d4cb802109a58018fc0fb7c1967b9500e
KARTIK-44 <kartikpatilcs@gmail.com>
2025-08-26 14:06:38 +0530

    bit_manupalation

---
+++ BitManupalation/Even_odd.java
@@ -0,0 +1,21 @@
+package BitManupalation;
+
+public class Even_odd {
+    public static void EvenOdd(int n) {
+        int bitmask = 1;
+        if ((n & bitmask) == 0) {
+            // even
+            System.out.println(n + " is even number");
+        } else {
+            // odd
+            System.out.println(n + " is Odd number");
+        }
+    }
+
+    public static void main(String[] args) {
+        EvenOdd(3);
+        EvenOdd(6);
+
+    }
+
+}
