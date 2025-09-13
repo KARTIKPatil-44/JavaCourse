abadd41c0efb4e88ffc06e353ee0f2771fe5c5be
KARTIK-44 <kartikpatilcs@gmail.com>
2025-09-13 19:15:58 +0530

    Problem3

--- BitManupalation/Problems.java
+++ BitManupalation/Problems.java
@@ -7,21 +7,38 @@ public class Problems {
 
     // Question 2
     // Count set Bits in a Number
-    public static int countSetBits(int n){
+    public static int countSetBits(int n) {
         int count = 0;
-        while ( n > 0) {
-            if((n & 1) != 0){ // check LSB
+        while (n > 0) {
+            if ((n & 1) != 0) { // check LSB
                 count++;
             }
             n = n >> 1;
-            
+
         }
         return count;
     }
 
+    // Question 3
+    // Check the fast Exponentiation (x^n)
+    public static int fastExp(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ans = ans * a;
+
+            }
+            a = a * a;
+            n = n >> 1;
+        }
+        return ans;
+
+    }
+
     public static void main(String[] args) {
         // System.out.println(PowerOfTowOrNot(15));
-        System.out.println(countSetBits(15));
+        // System.out.println(countSetBits(15));
+        System.out.println(fastExp(5, 3));
 
     }
 
